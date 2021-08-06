7162a31bfe5085ab828d6f6f802ec6112c73bbed
unknown <faridun.nk@gmail.com>
2021-08-06 23:39:31 +0500

    created console .net core project

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lesson2App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.Write("Введите переменную A: ");
+            int A=int.Parse(Console.ReadLine());
+            Console.Write("Введите переменную B: ");
+            int B=int.Parse(Console.ReadLine());
+            if (A>B) 
+            {
+               B=A;
+               Console.WriteLine(B);
+            }
+            else if (A<B)
+            {
+               A=B;
+               Console.WriteLine(A);
+            } 
+            
+            else
+            {
+               A=0;
+               B=0;
+               Console.WriteLine(A);
+               Console.WriteLine(B);    
+            }
+        }
+    }
+}
